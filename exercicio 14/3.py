cb3cd62f63f35e0d586e1a5189257e6f193de13f
Carl0sAndre <160773664+Carl0sAndre@users.noreply.github.com>
2024-09-11 21:36:44 -0300

    Add files via upload

---
+++ exercicio 14/3.py
@@ -0,0 +1,5 @@
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite outro número inteiro: "))
+num3 = int(input("Digite outro número inteiro: "))
+
+print(num1, num2, num3, sep='/')
---
+++ exercicio 14/4.py
@@ -0,0 +1,6 @@
+string1 = input("Digite a primeira string: ")
+string2 = input("Digite a segunda string: ")
+
+print(string1, end=' ') 
+print(string2)
+print()
